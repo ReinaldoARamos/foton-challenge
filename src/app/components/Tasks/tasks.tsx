1d7a8aa28d788e15ecb8ecaf4ffd19fe81b0ef97
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-11-26 20:41:26 -0300

    criando o search das tasks

--- src/app/components/Tasks/tasks.tsx
+++ src/app/components/Tasks/tasks.tsx
@@ -1,5 +1,6 @@
 import { api } from "@/app/lib/axios";
 import { Trash } from "phosphor-react";
+import { useEffect, useState } from "react";
 
 interface TaskProps {
   id:string
@@ -8,20 +9,30 @@ interface TaskProps {
 }
 
 
-async function HandleDelete(id : string) {
-  try {
-    console.log("deletando " + id);
-    const response = await api.delete(`${id}`);
-  
-
-  } catch (error) {
-    console.error("Validation error:", error);
-  }
-}
 
 
 
 export function Tasks({description, title, id} : TaskProps) {
+  const [deleteTriggered, setDeleteTriggered] = useState(false);
+
+  async function HandleDelete(id : string) {
+    try {
+      console.log("deletando " + id);
+      const response = await api.delete(`${id}`);
+      setDeleteTriggered(true);
+  
+    } catch (error) {
+      console.error("Validation error:", error);
+    }
+  }
+  
+  useEffect(() => {
+    if (deleteTriggered) {
+      // Reload the page when delete is triggered
+      window.location.reload();
+    }
+  }, [deleteTriggered]);
+  
   return (
     <div className="bg-task px-4 py-4  rounded-md">
    
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { MagnifyingGlass } from "phosphor-react";
 import { Tasks } from "./components/Tasks/tasks";
 import AddFeather from "./components/addFeather/addFeather";
@@ -6,69 +6,90 @@ import { UseRedirect } from "./Hooks/useRedirect";
 import { Avatar } from "./components/Avatar/avatar";
 import { useQuery } from "@tanstack/react-query";
 import { api } from "./lib/axios";
-import Image from 'next/image'
-interface Tasks  {
-  id: string
-  title: string
-  Description: string
+import Image from "next/image";
+import { useState } from "react";
+interface Tasks {
+  id: string;
+  title: string;
+  Description: string;
 }
 export default function TaskList() {
-
- 
+  const [search, setSearch] = useState<string>("");
   
-  const redirectTo = UseRedirect()
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newText = event.target.value;
+    console.log(newText)
+    setSearch(event.target.value);
+  };
+  const redirectTo = UseRedirect();
 
   const { isLoading, data } = useQuery<Tasks[]>({
     queryKey: ["Tasks"],
     queryFn: async () => {
       const response = await api.get(`/tasks`);
-      console.log(response.data)
+      console.log(response.data);
       return response.data;
     },
   });
- 
+
+  const filteredTasks = data?.filter(
+    (task) =>
+      task.title.toLowerCase().includes(search.toLowerCase()) ||
+      task.Description.toLowerCase().includes(search.toLowerCase())
+  );
+
+  console.log(filteredTasks)
   return (
     <div className="relative p-3.5">
       <div className="pb-4">
-       <div className="flex justify-between items-center">
-       <h1 className="text-header text-3xl font-bold font-mono space-y-1.5">
-          Hello, Reinaldo
-        </h1>
-       
-       
-        <Avatar src={"https://avatars.githubusercontent.com/u/55931337?v=4"}   />
-       </div>
-        <span className="font-mono text-header text-sm font-medium">
+        <div className="flex items-center justify-between">
+          <h1 className="space-y-1.5 font-mono text-3xl font-bold text-header">
+            Hello, Reinaldo
+          </h1>
+
+          <Avatar
+            src={"https://avatars.githubusercontent.com/u/55931337?v=4"}
+          />
+        </div>
+        <span className="font-mono text-sm font-medium text-header">
           Check your tasks! 👇{" "}
         </span>
       </div>
-      <div className="bg-gray-100 mb-5 rounded-md py-2.5 space-x-1 pl-5 gap flex items-center border border-transparent transition duration-200 hover:border-gray-400">
+      <div className="gap mb-5 flex items-center space-x-1 rounded-md border border-transparent bg-gray-100 py-2.5 pl-5 transition duration-200 hover:border-gray-400">
         <MagnifyingGlass color="#BCBCBC" size={22} />
         <input
           placeholder="Search..."
-          className="w-full text-xl outline-none text-input-text bg-gray-100"
+          className="w-full bg-gray-100 text-xl text-input-text outline-none"
+          onChange={handleSearch}
         />
       </div>
-      {data?.length ? (
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : data?.length ? (
         <div className="space-y-5">
-        {data?.map((task) => (
-          <Tasks description={task.Description} title={task.title} key={task.title} id={task.id} />
-        ))}
-        </div>
-      ) : <div className="flex justify-center items-center flex-col">
-         <Image
-              src={"/images/image 2.png"}
-              alt=""
-              width={261}
-              height={222}
+           {filteredTasks?.map((task) => (
+            <Tasks
+              description={task.Description}
+              title={task.title}
+              key={task.title}
+              id={task.id}
             />
-            <h2 className="text-header text-2xl text-center pt-6 font-bold font-mono space-y-1.5">
-          Você ainda não possui tarefas! 
-        </h2>
-        </div>}
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center">
+          <Image src={"/images/image 2.png"} alt="" width={261} height={222} />
+          <h2 className="space-y-1.5 pt-6 text-center font-mono text-2xl font-bold text-header">
+            Você ainda não possui tarefas!
+          </h2>
+        </div>
+      )}
 
-      <button className="fixed bottom-4 right-4 bg-header text-white rounded-full " onClick={() => redirectTo('/newtask')}>
-        <div className="p-6 flex justify-center items-center">
+      <button
+        className="fixed bottom-4 right-4 rounded-full bg-header text-white "
+        onClick={() => redirectTo("/newtask")}
+      >
+        <div className="flex items-center justify-center p-6">
           <AddFeather />
         </div>
       </button>
