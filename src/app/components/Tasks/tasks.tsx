b4e19d9f62bb4f5e0cebac0a4c4bc6ac2204b4c9
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-11-28 17:28:38 -0300

    criando a estilização caso a  task esteja concluida

--- src/app/components/Tasks/tasks.tsx
+++ src/app/components/Tasks/tasks.tsx
@@ -1,62 +1,71 @@
+"use client";
 import { api } from "@/app/lib/axios";
 import { Trash } from "phosphor-react";
 import { useEffect, useState } from "react";
+import * as Checkbox from "@radix-ui/react-checkbox";
+import { CheckIcon } from "lucide-react";
 
 interface TaskProps {
-  id:string
-  title: string
-  description: string
+  id: string;
+  title: string;
+  description: string;
 }
 
-
-
-
-
-export function Tasks({description, title, id} : TaskProps) {
+export function Tasks({ description, title, id }: TaskProps) {
   const [deleteTriggered, setDeleteTriggered] = useState(false);
-  const [isDeleted, setIsDeleted] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
 
-  async function HandleDelete(id : string) {
+  async function HandleDelete(id: string) {
     try {
       console.log("deletando " + id);
       const response = await api.delete(`${id}`);
       setDeleteTriggered(true);
-  
     } catch (error) {
       console.error("Validation error:", error);
     }
   }
-  
+
   useEffect(() => {
     if (deleteTriggered) {
-      // Reload the page when delete is triggered
       window.location.reload();
     }
   }, [deleteTriggered]);
 
-  useEffect(() => {
-    if (isDeleted) {
-      // Add a delay before fading away and moving to the right
-      setTimeout(() => {
-        setIsDeleted(false); // Reset the state to reuse the component
-      }, 1000); // Adjust the duration as needed
-    }
-  }, [isDeleted]);
   return (
     <div
-      className={`${
-        isDeleted ? 'slideLeftAndFade' : ''
-      } bg-task px-4 py-4 rounded-md transition-transform`}
+      className={`flex items-center  gap-3 rounded-md  bg-task px-4 py-4  ${
+        isChecked ? "line-through opacity-90" : ""
+      }`}
     >
-      <div className="flex justify-between">
-        <h1 className="pb-1.5 text-xl text-white font-bold">{title}</h1>
-        <Trash
-          size={24}
-          className="text-red-700 cursor-pointer"
-          onClick={() => HandleDelete(id)}
-        />
+      <Checkbox.Root
+        checked={isChecked}
+        //@ts-ignore
+        onCheckedChange={(state) => setIsChecked(state)}
+        className="shadow-blackA4 hover:bg-violet3 flex h-6 w-6 appearance-none items-center justify-center rounded-full  bg-white shadow-[0_2px_10px] outline-none focus:shadow-[0_0_0_2px_black]"
+      >
+        <Checkbox.Indicator className="text-violet11 ">
+          <CheckIcon />
+        </Checkbox.Indicator>
+      </Checkbox.Root>
+      <div className="flex w-full flex-col ">
+        <div className=" flex items-center justify-between">
+          <h1
+            className={`pb-1.5 text-xl font-bold ${
+              isChecked ? "text-white  line-through opacity-90 " : "text-white"
+            }`}
+          >
+            {title}
+          </h1>
+          
+        </div>
+        <div className={`text-xs font-medium text-white `}>{description}</div>
+        
       </div>
-      <div className="text-white text-xs font-medium">{description}</div>
+      <Trash
+            size={24}
+            className="cursor-pointer text-red-700"
+            onClick={() => HandleDelete(id)}
+          />
     </div>
   );
 }
